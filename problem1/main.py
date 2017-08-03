6934b0d46f2a7b3887dc090e94799abef0d7642d
Ali Hussain <alihussain@Alis-Mac-mini.local>
2017-08-03 17:20:26 -0500

    Project Euler Simple find multiples

---
+++ problem1/main.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3
+
+def find_multiples(factors, start, end):
+    multiples = []
+    for number in range(start, end+1):
+        for factor in factors:
+            if number % factor == 0:
+                multiples.append(number)
+                break
+    return multiples
+
+
+multiples = find_multiples([3,5], 1, 999)
+print( sum(multiples))
