f3d0a0c877236b06d89171b145dc30edaa24d008
Ali Hussain <alihussain@Alis-Mac-mini.local>
2017-08-03 20:04:35 -0500

    Caculate prime factors

---
+++ library/prime_factors.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python3
+
+import math
+from primes import PrimeCalculator
+
+class Primefactors:
+    def get_prime_factors(num):
+        prime_factors = {}
+        primes = PrimeCalculator()
+        for prime in primes:
+            if prime > num/2:
+                break
+            num_divided = num
+            while num_divided % prime == 0:
+                prime_factors[prime] = prime_factors.get(prime,0)+1
+                num_divided = num_divided/prime
+            # End when >sqrt, self and 1 are not prime factors
+        return prime_factors
+
+if __name__ == "__main__":
+    for num in range(1,20):
+        result = Primefactors.get_prime_factors(num)
+        print("Prime factors for %d are:" % num)
+        print(result)
