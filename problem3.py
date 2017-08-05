609e9442af62d4b48a4e23c961364fd99ed5750b
Ali Hussain <ali@flux7.com>
2017-08-05 01:21:26 -0500

    SOlve problem 3

---
+++ problem3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+from EulerLibrary.PrimeFactors import PrimeFactors
+
+number = 600851475143
+prime_factors = PrimeFactors.get_prime_factors(number)
+print(max(prime_factors.keys()))
