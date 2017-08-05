9dc0dc6b6cad326b8a91eafc54136e43a29ff307
Ali Hussain <ali@flux7.com>
2017-08-05 11:36:02 -0500

    Add error messages to assertions

--- tests/test_PrimeFactors.py
+++ tests/test_PrimeFactors.py
@@ -30,13 +30,16 @@ class TestPrimeFactors(unittest.TestCase):
     def test_known_primes(self):
         for num in range(1,21):
             result = PrimeFactors.get_prime_factors(num)
-            self.assertEqual(result,TestPrimeFactors.preset_factors[num])
+            self.assertEqual(result,TestPrimeFactors.preset_factors[num],
+                            msg="%s did not match golden values of %s for number %d" %
+                                (str(result),str(TestPrimeFactors.preset_factors[num]),num))
 
     def test_factor_recreate(self):
         for num in range(1,200):
             prime_factors = PrimeFactors.get_prime_factors(num)
             result = PrimeFactors.get_number(prime_factors)
-            self.assertEqual(result,num)
+            self.assertEqual(result,num,msg="Failed for number %d with factors %s and result %d" %
+                                            (num,str(prime_factors),result))
 
 if __name__ == '__main__':
     unittest.main()
