1be41740453571d2def2466603a38d735733e321
Ali Hussain <ali@flux7.com>
2017-09-10 10:57:54 -0500

    Boiler plate

--- problem15.py
+++ problem15.py
@@ -4,3 +4,15 @@ Starting in the top left corner of a 2×2 grid, and only being able to move to t
 there are exactly 6 routes to the bottom right corner.
 How many such routes are there through a 20×20 grid?
 '''
+import logging
+import argparse
+import os
+from EulerLibrary import Utilities
+from EulerLibrary.Factors import get_all_factors
+
+def arguments(parser):
+    parser.add_argument('--rows','-r',type=int,default=20,help='Number of rows in grid')
+    parser.add_argument('--columns','-c',type=int,default=20,help='Number of columns in grid')
+
+if __name__ == '__main__':
+    PARSED_ARGS = Utilities.initialize(arguments)
