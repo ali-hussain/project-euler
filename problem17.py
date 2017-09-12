50974610acdcb13f7cf0cd05321a347b1f95d527
Ali Hussain <ali@flux7.com>
2017-09-11 19:21:08 -0500

    Problem solved

--- problem17.py
+++ problem17.py
@@ -15,11 +15,26 @@ British usage.
 
 import logging
 import argparse
+import inflect
+import re
 from EulerLibrary import Utilities
 
 def arguments(parser):
     parser.add_argument('--number','-n',type=int,default=1000,
         help='Highest number we want to go to')
 
+def strip_nonalpha(word_num):
+    return strip_nonalpha.regex_pattern.sub('',word_num)
+strip_nonalpha.regex_pattern = re.compile('[^a-zA-Z]')
+
 if __name__ == '__main__':
     PARSED_ARGS = Utilities.initialize(arguments)
+    p = inflect.engine()
+    word_nums = []
+    processed_word_nums = []
+    for number in range(1,PARSED_ARGS.number+1): # Problem asks for inclusive
+        word_num = p.number_to_words(number)
+        word_nums.append(word_num)
+        processed_word_num = strip_nonalpha(word_num)
+        processed_word_nums.append(processed_word_num)
+    print( sum([len(num_string) for num_string in processed_word_nums]))
